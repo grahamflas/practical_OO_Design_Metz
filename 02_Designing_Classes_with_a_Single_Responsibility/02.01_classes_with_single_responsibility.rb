5e4dc2d269a389493a247d754afcc0fcc7c89438
Graham Flaspoehler <grahamflas@gmail.com>
2020-03-15 19:21:58 -0400

    Rename files

---
+++ 02_Designing_Classes_with_a_Single_Responsibility/02.01_classes_with_single_responsibility.rb
@@ -0,0 +1,22 @@
+=begin
+_______________ Designing Classes with a Single Responsibility _______________ 
+
+Goals when designing an application
+  1. Model using classes so that the app does what it needs to do right now
+  2. Arrange code such that it's easy to change later
+_
+
+### _____ Deciding What Belongs in a Class _____
++ Challenge isn't writing the code, it's organizing it. 
+
++ Code that is easy to change:
+  + has no unexpected side-effects
+  + small changes in specs require small changes in code
+  + is reuseable
+  + incorporates changes by adding code that is easy to change
+_
+
+
+  
+=end
+
