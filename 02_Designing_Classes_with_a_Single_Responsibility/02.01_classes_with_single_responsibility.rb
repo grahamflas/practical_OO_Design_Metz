f1b7276f8a7e3fa4a968a6917ec7502f5d036c30
Graham Flaspoehler <grahamflas@gmail.com>
2020-03-18 15:42:54 -0400

    Add notes on Single Responsibility

--- 02_Designing_Classes_with_a_Single_Responsibility/02.01_classes_with_single_responsibility.rb
+++ 02_Designing_Classes_with_a_Single_Responsibility/02.01_classes_with_single_responsibility.rb
@@ -6,7 +6,7 @@ Goals when designing an application
   2. Arrange code such that it's easy to change later
 _
 
-### _____ Deciding What Belongs in a Class _____
+###_____ Deciding What Belongs in a Class _____
   + Challenge isn't writing the code, it's organizing it.
   + Methods need to be grouped into classes that define a virtual world, will shape the way you think about your application
   + Impossible to get it all right from the outset, so design with future changes in mind 
@@ -42,15 +42,40 @@ _
       + Low gear: feet pedal many times to make the wheel rotate once
       + High gear: each complete pedal leads wheel to spin multiple times
 
-    + Compare gears by comparing the ratio of the number of teeth on the chainring (the part the pedals turn) to the number of teeth on the cog (the part on the wheel)
-      + chainring = 52 (teeth)
-      + cog = 11
-      + ratio = chainring / cog #=> 4.7272727273 rotatations of the wheel each time 
+  + Compare gears by comparing the ratio of the number of teeth on the chainring (the part the pedals turn) to the number of teeth on the cog (the part on the wheel)
+    + chainring = 52 (teeth)
+    + cog = 11
+    + ratio = chainring / cog #=> 4.7272727273 rotatations of the wheel each time 
 
-    + Create a gear class
+  + Create a Gear class
+  + Extend Gear class to account for differences in rim and tire size
+    + Note that changing the number of arguments (adding tire and rim) introduces a bug by breaking all existing callers of the method
+  
+  + Creating the #gear_inches method required adding attributes to the model, which changed the number of arguments you need to pass to the #inititalize method, breaking all existing callers of that method.  This is fine for now, but you could easily imagine a number of calculator methods needing to be added as the application evolves.
+### ___________________________________
+
+### _____ Why Signle Responsibility Matters _____
+
+  + Applications that are conducive to change are made up of reusable classes that can be selected and assembled in unanticipated ways
+  + Reusable classes have 1) well-defined behavior and 2) few entanglements
+
+  + A class that has more than one responsibility is difficult to reuse, becuase the responsibilities are likely entangled within the class, making it difficult to use only the behaviour you need
+    + Result: 
+      + 1) Violate DRY by duplicating your code -- hard to maintain, easy to introduce bugs
+      + 2) You may end up reusing the entire over-burdened class in order to use only part of its responsibilities; possibility of the class needing to change, which may break the application in an unexpected way.
+
+  + The Gear class defined below has more than one responsibility
+    + How to tell
+      1) Imagine you can interrogate the class, ask each of its methods as a question
+          + Gear, what is your ratio?  #=> makes sense
+          + Gear, what is your tire?  #=> DOES NOT make sense
+      2) Describe the class in a single sentence without using AND or OR
+        + "The Gear class calculates the effect that a gear has on a bicycle" --> rim and tire don't belong here
+### ___________________________________
+
 =end
 
-class Gear
+class Gear_That_Does_Too_Much
   attr_reader :chainring,
               :cog,
               :rim,
@@ -71,3 +96,4 @@ class Gear
     ratio * (rim + (2 * tire))
   end
 end
+
